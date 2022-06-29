d3ccd2eefa8ef0b08e51fa894dfd58255cba081c
Nitesh377 <nitesh.sharma@goguardian.com>
2022-06-29 17:22:54 +0530

    code refactored

--- oneroster-import-js/src/index.js
+++ oneroster-import-js/src/index.js
@@ -7,6 +7,16 @@ import InMemoryStorage from "./Storage/InMemoryStorage.js";
 import Organisation from "./Entity/Organisation.js";
 import User from "./Entity/User.js";
 import ClassRoom from "./Entity/ClassRoom.js";
+import AcademicSession from "./Entity/AcademicSession.js";
+import Category from './Entity/Category.js';
+import ClassResource from './Entity/ClassResource.js'
+import Course from './Entity/Course.js'
+import CourseResource from './Entity/CourseResource.js'
+import Demographic from "./Entity/Demographic.js";
+import Enrollment from "./Entity/Enrollment.js";
+import LineItem from "./Entity/LineItem.js"
+import Resource from "./Entity/Resource.js";
+import Result from "./Entity/Result.js"
 
 
-export {FileHandler, ImportService, EntityRepository, RelationConfigFactory, CsvStorage, InMemoryStorage, Organisation, User, ClassRoom}
+export {FileHandler, ImportService, EntityRepository, RelationConfigFactory, CsvStorage, InMemoryStorage, Organisation, User, ClassRoom, AcademicSession, Category, ClassResource, Course, CourseResource, Demographic, Enrollment, LineItem, Resource, Result}
